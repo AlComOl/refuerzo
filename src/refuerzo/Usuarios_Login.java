6338ba8588e39016a851253415dcb090cd924687
AlComOl <alvcomoli@alu.edu.gva.es>
2023-09-12 21:27:38 +0200

    Funciona

--- src/refuerzo/Usuarios_Login.java
+++ src/refuerzo/Usuarios_Login.java
@@ -8,7 +8,7 @@ public class Usuarios_Login {
 		//Atributos
 	private String	nombre;
 	private String contrasena;
-	private String menu;
+	private String pantalla;
 		//Constructores
 	     public Usuarios_Login() {
 		
@@ -17,40 +17,64 @@ public class Usuarios_Login {
 			nombre=n;
 			contrasena=c;
 		}
+		
 		//Metodos
+		
 		//establece nombre
 		public void setNombre(String n) {
 			nombre=n;
 		}
+		
 		//devuelve nombre
 		public String getNombre() {
 			return nombre;
 		}
+		
 		//establece contraseña	
 		public void setContasena(String c) {
 			contrasena=c;
 		}
+		
 		//devuelve contraseña
 		public String getContrasena() {
 			return contrasena;
 		}
 		
-		public String menu() {
+		
+		public String pantalla() {
+			System.out.println("Elige una opcion");
 			System.out.println("0 --> Darse de Alta");
 			System.out.println("1 --> Ver cuenta");
 			System.out.println("2 --> Borrar cuenta");
 			System.out.println("3 --> Salir");
-			return menu;
+			System.out.println("Elige una opcion");
+			
+			return pantalla;
 			
 		}
-	    public void setreset() {
+		
+		//resetear nombre
+		
+		public void setResetNombre() {
+	    	 
 	    	nombre= null;
+	    }
+		
+		//devuelve nombre reseteado
+		public String getResetNombre() {
+	    	
+	    	return getResetNombre();
+	    }
+		
+		//borra contraseña
+	    public void setResetContrasena() {
+	    	
 	    	contrasena= null;
 	    }
+	    // devuelve la contraseña
+	    public String getresetContrasena() {
 	    
-	    public String getreset() {
-	    
-	    	return  getreset();
+	    	return  getresetContrasena();
 	    }
 	
 		
@@ -66,63 +90,53 @@ public class Usuarios_Login {
 		
 			
 		
-		u1.menu();
-
+		
+		
+		
+		int men = 0;
+		do {
+			u1.pantalla();
+		
+		
 		int menu=entrada.nextInt();
-		int menu1;
+	
 		switch (menu) {
-		
+	
 		case 0:
-			System.out.println("Introduce el nombre para registrarte");
+			System.out.println("Estas Dandote de alta en el sistema \n Ingresa tu nombre");
 			u1.setNombre(entrada.next());
-			System.out.println("Guardado");
-			System.out.println("Introcuce la contraseña, apuntala para no olvidarla");
+			System.out.println(" Estas Dandote de alta en el sistema  \n Ingresa tu contraseña");
 			u1.setContasena(entrada.next());
-			System.out.println("Guardado");
-			u1.menu();
-			entrada.nextInt();
-			
+			System.out.println("Te has dado de alta satisfactoriamente");
+			//u1.pantalla();
+			//entrada.nextInt();
+		break;
 		case 1:
-		    System.out.println("nombre:" + u1.getNombre());	
-		    System.out.println("contrasena:" +u1.getContrasena());
-		    u1.menu();
-		    break;
+			System.out.println("comprobacion de credenciales");
+			System.out.println("USER : " + u1.getNombre()); 
+			System.out.println("PASSWORD : " +u1.getContrasena());	
+			//u1.pantalla();
+			//entrada.nextInt();
+		break;
 		case 2:
-			System.out.println("Estas seguro que quieres borrar la cuenta? 1--->SI  \n 2 ---> NO");
+			System.out.println("Borramos cuentas");
+			u1.setResetContrasena();
+			u1.setResetNombre();
+			System.out.println("Has borrado los crecenciales satisfatoriamente");
 			
-			 menu1=entrada.nextInt();
-			
-			switch (menu1) {
-			case 1:
-				System.out.println("SI");
-				u1.setreset();
-				u1.menu();
-				entrada.nextInt();
-				break;
-			case 2:
-				System.out.println("NO");
-				
-				u1.menu();
-			case 3:
-				System.out.println("Comprobar si valor de nombre y contraseña");
-				u1.getreset();
-			default:
-				break;
-			}
-				
-				
+		break;
 		case 3:
-			 System.out.println("Adios");
-		default:
-			break;
-		 
-		
+			System.out.println("Adios");
+			men=entrada.nextInt();
 			
+			
+		break;
+		}
 		
+		}while(men !=3);
+		entrada.close();
 		
 	}
-		
-		}
 }
 
 	
