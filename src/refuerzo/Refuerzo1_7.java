67d2f1a286f117a3cc0adcb4a260d1d03f2e7e84
AlComOl <alvcomoli@alu.edu.gva.es>
2023-10-10 22:26:06 +0200

    1_7

---
+++ src/refuerzo/Refuerzo1_7.java
@@ -0,0 +1,35 @@
+package refuerzo;
+/*****************************************************************
+
+Autor: Álvaro Comenge
+
+Fecha:9/10/23
+
+Descripción:
+
+	Escribe un programa que pida un número entero al usuario, y muestre por pantalla
+	una cuenta atrás hasta cero, seguido de un “¡BOOOM!”.
+	Cuidado: Una vez lo tengas, ¿funciona correctamente si el usuario introduce un
+	número negativo? De no ser así, arréglalo
+
+
+******************************************************************/
+import java.util.*;
+
+public class Refuerzo1_7 {
+
+
+	public static void main(String[] args) {
+			Scanner entrada= new Scanner(System.in);
+			
+			System.out.println("Introdue un numero entero");
+			int n = entrada.nextInt();
+			
+			for(;n>0;n--) {
+				System.out.println(n);
+				
+			}
+	}
+	
+
+}
